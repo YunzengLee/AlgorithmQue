936fefc31555a6226c35eee3b2f068b2c1abda36
liyunzeng <17865197576@163.com>
2020-09-10 19:36:40 +0800

    单调栈

---
+++ LeetcodeCollection/exam9_10.py
@@ -0,0 +1,36 @@
+class Solution:
+    def lastRemaining(self , n , m ):
+        # write code here
+        persons = [i for i in range(n)]
+        startidx = 0
+        while len(persons) != 1:
+            choosed_idx = (startidx + m - 1) % len(persons)
+            persons.pop(choosed_idx)
+            startidx = choosed_idx
+        return persons[0]
+
+class Solution1:
+    def make_cancellation(self , content , bomb ):
+        # write code here
+        res = ''
+        i = 0
+        while i < len(content):
+            if i+1<len(content) and content[i] == content[i+1]:
+                if content[i] != bomb:
+                    i += 2
+                else:
+                    if res:
+                        res = res[:-1]
+                    i += 3
+            else:
+                res = res + content[i]
+                i+=1
+            print(i,res)
+if __name__ =='__main__':
+    # a=Solution1()
+    # res=a.make_cancellation('112','2')
+    # print(res)
+    c=[(1,2),(3,4),(5,6)]
+    d=list(zip(*c))
+    print(d)
+    pass
--- LeetcodeCollection/fuzhu_stack_or_queue.py
+++ LeetcodeCollection/fuzhu_stack_or_queue.py
@@ -1,3 +1,36 @@
+'''
+单调栈
+'''
+
+'''
+leetcode 739 每日温度
+请根据每日 气温 列表，重新生成一个列表。对应位置的输出为：要想观测到更高的气温，至少需要等待的天数。如果气温在这之后都不会升高，请在该位置用 0 来代替。
+例如，给定一个列表 temperatures = [73, 74, 75, 71, 69, 72, 76, 73]，你的输出应该是 [1, 1, 4, 2, 1, 1, 0, 0]。
+提示：气温 列表长度的范围是 [1, 30000]。每个气温的值的均为华氏度，都是在 [30, 100] 范围内的整数。
+注意：栈内存的是下标，不是温度值。要灵活运用!!!!!!!!!
+'''
+class Solution_leet739(object):
+    def dailyTemperatures(self, T):
+        """
+        :type T: List[int]
+        :rtype: List[int]
+        """
+        res = [0 for i in range(len(T))]
+        stack = []
+        cur_idx = 0
+        while cur_idx<len(T):
+            if not stack:
+                stack.append(cur_idx)
+            else:
+                while stack and T[stack[-1]]<T[cur_idx]:
+                    pop_idx = stack.pop()
+                    res[pop_idx] = cur_idx - pop_idx
+                stack.append(cur_idx)
+            cur_idx+=1
+        return res
+
+
+
 ''''''
 '''面试题59-ii 队列的最大值'''
 # 实现一个队列类 可以以O（1）的复杂度取最大值
